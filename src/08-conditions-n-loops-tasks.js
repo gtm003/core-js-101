ae906f4c97f89ffce441d2eeeb6a501bd218aaa5
Grigorovich Tatyana <gtm003@gmail.com>
2020-12-14 16:20:42 +0300

    feat: add solution function getComposition(f, g) and function getPowerFunction(exponent)

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -169,8 +169,11 @@ function doRectanglesOverlap(r1, r2) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const dimX = Math.abs(circle.center.x - point.x);
+  const dimY = Math.abs(circle.center.y - point.y);
+  const dimFromPointToCenter = Math.sqrt(dimX ** 2 + dimY ** 2);
+  return dimFromPointToCenter < circle.radius;
 }
 
 
